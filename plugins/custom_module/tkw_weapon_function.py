d59d0897cd09afb1fca1d2c7572ea7d2923bbb18
poeynus <poeynus@gmail.com>
2024-05-25 22:16:08 +0900

    로직 수정

--- plugins/custom_module/tkw_weapon_function.py
+++ plugins/custom_module/tkw_weapon_function.py
@@ -270,10 +270,10 @@ def check_weapon_category(weapon_short_name, weapon_category):
         return "Special weapons"
     elif weapon_short_name in grenade_weapons:
         return "Grenade launcher"
-    elif weapon_category == "Revolver":
-        return "Handgun"
     elif weapon_short_name == "MTs-255-12":
         return "Shotgun"
+    elif weapon_category == "Revolver":
+        return "Handgun"
     else:
         return weapon_category
 
