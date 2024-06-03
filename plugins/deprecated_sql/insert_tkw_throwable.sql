51cc80c62b8b635022ab5c611eb959649ebbb7d1
poeynus <poeynus@gmail.com>
2024-06-04 08:43:52 +0900

    필요없는 코드 제거

--- plugins/deprecated_sql/insert_tkw_throwable.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO tkw_throwable_info (
-    throwable_id, throwable_name, throwable_short_name, throwable_image, throwable_category,
-    throwable_fuse, throwable_min_explosion_distance, throwable_max_explosion_distance,
-    throwable_fragments, throwable_update_time)
-VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s);
