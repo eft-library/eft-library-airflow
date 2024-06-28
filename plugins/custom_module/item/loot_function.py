26e44928f10b1808d6646e1974b9ab05f3c4e1ae
poeynus <poeynus@gmail.com>
2024-06-29 07:02:39 +0900

    전리품 한글 카테고리 추가

--- plugins/custom_module/item/loot_function.py
+++ plugins/custom_module/item/loot_function.py
@@ -7,17 +7,54 @@ def process_loot(item):
     """
 
     id = item.get("id")
-    name = item.get("name")
+    name_en = item.get("name")
+    name_kr = get_loot_kr(name_en)
     short_name = item.get("shortName")
     image = item.get("image512pxLink")
-    category = item["category"].get("name") if item.get("category") else None
+    category_en = item["category"].get("name") if item.get("category") else None
+    category_kr = get_category_kr(category_en)
     update_time = pendulum.now("Asia/Seoul")
 
     return (
         id,
-        name,
+        name_en,
+        name_kr,
         short_name,
         image,
-        category,
+        category_en,
+        category_kr,
         update_time,
     )
+
+
+def get_category_kr(category):
+    """
+    카테고리 한글
+    """
+
+    category_dict = {
+        "Others": "기타",
+        "Building materials": "건축 자재",
+        "Electronics": "전자 제품",
+        "Energy elements": "에너지 관련 용품",
+        "Flammable materials": "가연성 물질",
+        "Household materials": "가정용품",
+        "Medical supplies": "의료용품",
+        "Tools": "도구",
+        "Valuables": "귀중품",
+        "Info items": "정보 아이템",
+        "Special equipment": "특수 장비",
+    }
+
+    if category in category_dict:
+        return category_dict[category]
+    else:
+        return category
+
+
+def get_loot_kr(name):
+    """
+    전리품 한글 이름
+    """
+
+    return name
--- plugins/sql/upsert_tkl_loot.sql
+++ plugins/sql/upsert_tkl_loot.sql
@@ -1,16 +1,20 @@
 INSERT INTO tkl_loot (
     id,
-    name,
+    name_en,
+    name_kr,
     short_name,
     image,
-    category,
+    category_en,
+    category_kr,
     update_time
 ) VALUES (
-    %s, %s, %s, %s, %s, %s
+    %s, %s, %s, %s, %s, %s, %s, %s
 )
 ON CONFLICT (id) DO UPDATE SET
-    name = EXCLUDED.name,
+    name_en = EXCLUDED.name_en,
+    name_kr = EXCLUDED.name_kr,
     short_name = EXCLUDED.short_name,
     image = EXCLUDED.image,
-    category = EXCLUDED.category,
+    category_en = EXCLUDED.category_en,
+    category_kr = EXCLUDED.category_kr,
     update_time = EXCLUDED.update_time;
