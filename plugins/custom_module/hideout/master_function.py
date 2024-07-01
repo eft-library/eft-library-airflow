187ccfe881a73614dedc12b3ed2c53c7d3fae955
poeynus <poeynus@gmail.com>
2024-07-02 08:54:10 +0900

    master 디버그

--- plugins/custom_module/hideout/master_function.py
+++ plugins/custom_module/hideout/master_function.py
@@ -11,7 +11,6 @@ def process_master(item):
     construction_time = item.get("constructionTime")
     level_ids = get_level_ids(item.get("levels"))
     update_time = pendulum.now("Asia/Seoul")
-    print(item)
     return (
         id, name_en, image, construction_time, level_ids, update_time
     )
@@ -21,10 +20,10 @@ def get_level_ids(levels):
     """
     level id list 추출
     """
-    print(levels)
     level_ids = []
 
     for level in levels:
+        print(level)
         level_ids.append(level.get("id"))
 
     return level_ids
