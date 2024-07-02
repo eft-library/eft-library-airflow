2664345c6f727e6edd820f1fb809e7b74fc4d606
poeynus <poeynus@gmail.com>
2024-07-02 15:32:05 +0900

    오타 수정

--- plugins/custom_module/hideout/station_require_function.py
+++ plugins/custom_module/hideout/station_require_function.py
@@ -8,7 +8,7 @@ def process_station_require(level_id, item):
     id = item.get("id")
     level = item.get("level")
     name_en = item['station'].get('name') if item.get("station") else None
-    image = item['imageLink'].get('name') if item.get("station") else None
+    image = item['station'].get('imageLink') if item.get("station") else None
     update_time = pendulum.now("Asia/Seoul")
 
     return (
