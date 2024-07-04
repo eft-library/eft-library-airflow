d779546deb81d8d9f2c6e2f19e1233548a1e690e
poeynus <poeynus@gmail.com>
2024-07-05 08:32:37 +0900

    crafts 구조 변경

--- plugins/custom_module/hideout/crafts_function.py
+++ plugins/custom_module/hideout/crafts_function.py
@@ -8,20 +8,18 @@ def process_crafts(item):
     id = item.get("id")
     station_id = item['station'].get("id") if item.get("station") else None
     level = item.get("level")
-    name_en = get_name_list(item.get("rewardItems"))
+    name_en = get_name(item.get("rewardItems"))
 
     return (
         id, f"{station_id}-{level}", level, name_en, update_time
     )
 
 
-def get_name_list(rewards):
+def get_name(rewards):
     """
-    보상 리스트로 뽑기
+    보상 뽑기
+    리스트 형태이지만 하나 밖에 없음
     """
-    reward_list = []
-
-    for reward in rewards:
-        reward_list.append(reward["item"].get("name"))
-
-    return reward_list
+    if len(rewards) > 1:
+        return rewards[0]["item"].get("name")
+    return None
