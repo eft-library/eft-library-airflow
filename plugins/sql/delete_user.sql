82356b9622b08563285a2c3b44eab878995fd1e0
poeynus <poeynus@gmail.com>
2024-08-05 15:01:07 +0900

    사용자 계정 삭제 task 로직 수정

--- plugins/sql/delete_user.sql
+++ plugins/sql/delete_user.sql
@@ -1,2 +1,18 @@
-DELETE FROM TKL_USER_DELETE
-WHERE DELETE_END_TIME < NOW()
+BEGIN;
+
+DELETE FROM tkl_user_icon
+WHERE user_email IN (
+    SELECT email FROM tkl_user_delete
+    WHERE delete_end_time < NOW()
+);
+
+DELETE FROM tkl_user_quest
+WHERE user_email IN (
+    SELECT email FROM tkl_user_delete
+    WHERE delete_end_time < NOW()
+);
+
+DELETE FROM tkl_user_delete
+WHERE delete_end_time < NOW();
+
+COMMIT;
