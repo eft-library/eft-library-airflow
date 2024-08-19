6603a0b9a3cd7e0d822df830bc68fd35de6f5db6
poeynus <poeynus@gmail.com>
2024-08-19 14:26:49 +0900

    서버 rebuild dag 개발

---
+++ dags/dags_server_rebuild.py
@@ -0,0 +1,31 @@
+import json
+from airflow import DAG
+import datetime
+import pendulum
+from airflow.operators.http_operator import SimpleHttpOperator
+from airflow.models import Variable
+
+
+with DAG(
+    dag_id="dags_server_rebuild",
+    schedule="0 6 * * *",
+    start_date=pendulum.datetime(2021, 1, 1, tz="Asia/Seoul"),
+    catchup=False,
+    dagrun_timeout=datetime.timedelta(minutes=60),
+    tags=['server'],
+) as dag:
+
+    api_url = Variable.get('api_url')
+    rebuild_key = Variable.get('rebuild_key', default_var='')
+
+    server_rebuild_task = SimpleHttpOperator(
+        task_id='server_rebuild',
+        http_conn_id=None,
+        endpoint=f'{api_url}/server/rebuild',
+        method='POST',
+        data=json.dumps({"rebuild_key": rebuild_key}),
+        headers={"Content-Type": "application/json"},
+        dag=dag,
+    )
+
+    server_rebuild_task
