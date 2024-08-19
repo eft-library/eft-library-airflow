76efa52d133eb54151be22eff29556862cf3cb57
poeynus <poeynus@gmail.com>
2024-08-19 14:50:34 +0900

    timeout으로 인한 로직 수정

--- dags/dags_server_rebuild.py
+++ dags/dags_server_rebuild.py
@@ -1,9 +1,22 @@
 import json
+import requests
 from airflow import DAG
 import datetime
 import pendulum
-from airflow.operators.http_operator import SimpleHttpOperator
+from airflow.operators.python_operator import PythonOperator
 from airflow.models import Variable
+
+
+def trigger_server_rebuild(**kwargs):
+    rebuild_key = Variable.get('rebuild_key', default_var='')
+    url = Variable.get('api_url', default_var='')
+    headers = {"Content-Type": "application/json"}
+    data = {"rebuild_key": rebuild_key}
+
+    response = requests.post(f'{url}/api/server/rebuild', headers=headers, json=data, timeout=900)  # 타임아웃 15분 설정
+
+    if response.status_code != 200:
+        raise ValueError(f"Request failed with status {response.status_code}: {response.text}")
 
 
 with DAG(
@@ -14,17 +27,9 @@ with DAG(
     dagrun_timeout=datetime.timedelta(minutes=60),
     tags=['server'],
 ) as dag:
-
-    rebuild_key = Variable.get('rebuild_key', default_var='')
-
-    server_rebuild_task = SimpleHttpOperator(
+    server_rebuild_task = PythonOperator(
         task_id='server_rebuild',
-        http_conn_id='tkl_web',
-        endpoint='api/server/rebuild',
-        method='POST',
-        data=json.dumps({"rebuild_key": rebuild_key}),
-        headers={"Content-Type": "application/json"},
-        timeout=900,
+        python_callable=trigger_server_rebuild,
         dag=dag,
     )
 
