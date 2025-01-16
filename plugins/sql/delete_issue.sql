271b759419c1a2eae5844a12751f1f1e9068fc3a
poeynus <poeynus@gmail.com>
2025-01-16 10:05:35 +0900

    코드 정리 - 게시판 관련 전체 제거

--- plugins/sql/delete_issue.sql
+++
@@ -1,4 +0,0 @@
-delete from tkl_board_issue
-where board_id in (
-    select id from tkl_board_union where like_count < 10
-);
--- plugins/sql/upsert_issue.sql
+++
@@ -1,7 +0,0 @@
-insert into tkl_board_issue (board_id, board_type, create_time)
-select id, type, create_time
-from tkl_board_union
-where like_count >= 10
-ON CONFLICT (board_id) DO UPDATE SET
-    board_type  = EXCLUDED.board_type,
-    create_time = EXCLUDED.create_time;
