e6495e73ec76249cff74e96b365bbcc2aa9115c3
poeynus <poeynus@gmail.com>
2025-03-05 13:44:39 +0900

    airflow db dump 수정

--- plugins/custom_module/psql_function.py
+++ plugins/custom_module/psql_function.py
@@ -1,5 +1,5 @@
 
-sql_home_path = '/home/airflow/plugins/sql/'
+sql_home_path = '/opt/airflow/eft-library-airflow/plugins/sql/'
 
 def read_sql(sql_path):
     with open(sql_home_path + sql_path, 'r') as file:
