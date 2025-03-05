0127a781dbf496368684f0070a7ac09339f1282f
poeynus <poeynus@gmail.com>
2025-03-05 13:48:24 +0900

    price history 수정

--- plugins/custom_module/item_price/price_function.py
+++ plugins/custom_module/item_price/price_function.py
@@ -74,55 +74,55 @@ def mapping_trader(sell_for):
             "npc_id": "PEACE_KEEPER",
             "npc_name_en": "Peace Keeper",
             "npc_name_kr": "피스키퍼",
-            "npc_image": "/tkl_quest/npc/peacekeeper.webp"
+            "npc_image": "/eftlibrary/tkl_quest/npc/peacekeeper.webp"
         },
         "Mechanic": {
             "npc_id": "MECHANIC",
             "npc_name_en": "Mechanic",
             "npc_name_kr": "메카닉",
-            "npc_image": "/tkl_quest/npc/mechanic.webp"
+            "npc_image": "/eftlibrary/tkl_quest/npc/mechanic.webp"
         },
         "Prapor": {
             "npc_id": "PRAPOR",
             "npc_name_en": "Prapor",
             "npc_name_kr": "프라퍼",
-            "npc_image": "/tkl_quest/npc/prapor.webp"
+            "npc_image": "/eftlibrary/tkl_quest/npc/prapor.webp"
         },
         "Skier": {
             "npc_id": "SKIER",
             "npc_name_en": "Skier",
             "npc_name_kr": "스키어",
-            "npc_image": "/tkl_quest/npc/skier.webp"
+            "npc_image": "/eftlibrary/tkl_quest/npc/skier.webp"
         },
         "Fence": {
             "npc_id": "FENCE",
             "npc_name_en": "Fence",
             "npc_name_kr": "펜스",
-            "npc_image": "/tkl_quest/npc/fence.webp"
+            "npc_image": "/eftlibrary/tkl_quest/npc/fence.webp"
         },
         "Therapist": {
             "npc_id": "THERAPIST",
             "npc_name_en": "Therapist",
             "npc_name_kr": "테라피스트",
-            "npc_image": "/tkl_quest/npc/therapist.webp"
+            "npc_image": "/eftlibrary/tkl_quest/npc/therapist.webp"
         },
         "Jaeger": {
             "npc_id": "JAEGER",
             "npc_name_en": "Jaeger",
             "npc_name_kr": "예거",
-            "npc_image": "/tkl_quest/npc/jaeger.webp"
+            "npc_image": "/eftlibrary/tkl_quest/npc/jaeger.webp"
         },
         "Ragman": {
             "npc_id": "RAGMAN",
             "npc_name_en": "Ragman",
             "npc_name_kr": "래그맨",
-            "npc_image": "/tkl_quest/npc/ragman.webp"
+            "npc_image": "/eftlibrary/tkl_quest/npc/ragman.webp"
         },
         "Ref": {
             "npc_id": "REF",
             "npc_name_en": "Ref",
             "npc_name_kr": "레프",
-            "npc_image": "/tkl_quest/npc/ref.webp"
+            "npc_image": "/eftlibrary/tkl_quest/npc/ref.webp"
         },
         "Flea Market": {
             "npc_id": "FLEA_MARKET",
