945dbc8d8282731d231bd4d160cd5dbdd2be6e9c
poeynus <poeynus@gmail.com>
2025-03-05 13:56:52 +0900

    price history 수정

--- plugins/custom_module/item_price/price_function.py
+++ plugins/custom_module/item_price/price_function.py
@@ -128,7 +128,7 @@ def mapping_trader(sell_for):
             "npc_id": "FLEA_MARKET",
             "npc_name_en": "Flea Market",
             "npc_name_kr": "플리마켓",
-            "npc_image": "/tkl_main/fleamarket.webp"
+            "npc_image": "/eftlibrary/tkl_main/fleamarket.webp"
         },
     }
 
