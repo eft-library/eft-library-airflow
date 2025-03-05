31fd410b9c5bf4b585328156a384fa7f076d5131
poeynus <poeynus@gmail.com>
2025-03-05 09:27:21 +0900

    airflow db dump 수정

--- plugins/custom_module/data_dump_function.py
+++ plugins/custom_module/data_dump_function.py
@@ -24,7 +24,7 @@ def dump_script():
     return f"""
         source ~/.bashrc
         echo "Executing PostgreSQL Command - pg_dump"
-        pg_dump -h $DB -U tkl --inserts tkl > /home/latest_data/{today}_backup.sql
+        pg_dump -h $DB -U tkl --inserts tkl > /home/airflow/latest_data/{today}_backup.sql
         echo $?
         """
 
