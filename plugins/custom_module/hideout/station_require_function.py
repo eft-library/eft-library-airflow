83fb8dee282ad7c4dec811e391d78c4998fd7f15
poeynus <poeynus@gmail.com>
2025-03-24 16:00:17 +0900

    은신처 개발중

--- plugins/custom_module/hideout/station_require_function.py
+++ plugins/custom_module/hideout/station_require_function.py
@@ -8,9 +8,8 @@ def process_station_require(level_id, item):
     id = item.get("id")
     level = item.get("level")
     name_en = item['station'].get('name') if item.get("station") else None
-    image = item['station'].get('imageLink') if item.get("station") else None
     update_time = pendulum.now("Asia/Seoul")
 
     return (
-        id, level_id, level, name_en, image, update_time
+        id, level_id, level, name_en, update_time
     )
