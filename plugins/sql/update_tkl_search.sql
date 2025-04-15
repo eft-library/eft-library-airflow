e4928b58f4c8657b1546a212f186b6934d5ea59c
poeynus <poeynus@gmail.com>
2025-04-15 11:01:01 +0900

    sitemap 수정

--- plugins/sql/update_tkl_search.sql
+++ plugins/sql/update_tkl_search.sql
@@ -28,63 +28,63 @@ FROM (
                   SELECT '상인 : ' || tkl_npc.name_kr, '/quest', id, 'TRADER'
                   FROM tkl_npc
                   UNION ALL
-                  SELECT '무기 : ' || COALESCE(name_kr, name_en), '/itemd/detail/' || tkl_item.url_mapping, tkl_item.url_mapping, 'WEAPON'
+                  SELECT '무기 : ' || COALESCE(name_kr, name_en), '/item/' || tkl_item.url_mapping, tkl_item.url_mapping, 'WEAPON'
                   FROM tkl_item
                   WHERE category in ('Gun', 'Knife', 'Throwable')
                   UNION ALL
-                  SELECT '탄약 : ' || COALESCE(name_kr, name_en), '/itemd/detail/' || tkl_item.url_mapping, tkl_item.url_mapping, 'AMMO'
+                  SELECT '탄약 : ' || COALESCE(name_kr, name_en), '/item/' || tkl_item.url_mapping, tkl_item.url_mapping, 'AMMO'
                   FROM tkl_item
                   WHERE category = 'Ammo'
                   UNION ALL
-                  SELECT '방탄모 : ' || COALESCE(name_kr, name_en), '/itemd/detail/' || tkl_item.url_mapping, tkl_item.url_mapping, 'HEADWEAR'
+                  SELECT '방탄모 : ' || COALESCE(name_kr, name_en), '/item/' || tkl_item.url_mapping, tkl_item.url_mapping, 'HEADWEAR'
                   FROM tkl_item
                   WHERE category = 'Headwear'
                   UNION ALL
-                  SELECT '전술 조끼 : ' || COALESCE(name_kr, name_en), '/itemd/detail/'|| tkl_item.url_mapping, tkl_item.url_mapping, 'RIG'
+                  SELECT '전술 조끼 : ' || COALESCE(name_kr, name_en), '/item/'|| tkl_item.url_mapping, tkl_item.url_mapping, 'RIG'
                   FROM tkl_item
                   WHERE category = 'Rig'
                   UNION ALL
-                  SELECT '방탄 조끼 : ' || COALESCE(name_kr, name_en), '/itemd/detail/' || tkl_item.url_mapping, tkl_item.url_mapping, 'ARMOR_VEST'
+                  SELECT '방탄 조끼 : ' || COALESCE(name_kr, name_en), '/item/' || tkl_item.url_mapping, tkl_item.url_mapping, 'ARMOR_VEST'
                   FROM tkl_item
                   WHERE category = 'ArmorVest'
                   UNION ALL
-                  SELECT '헤드셋 : ' || COALESCE(name_kr, name_en), '/itemd/detail/' || tkl_item.url_mapping, tkl_item.url_mapping, 'HEADSET'
+                  SELECT '헤드셋 : ' || COALESCE(name_kr, name_en), '/item/' || tkl_item.url_mapping, tkl_item.url_mapping, 'HEADSET'
                   FROM tkl_item
                   WHERE category = 'Headset'
                   UNION ALL
-                  SELECT '가방 : ' || COALESCE(name_kr, name_en), '/itemd/detail/'|| tkl_item.url_mapping, tkl_item.url_mapping, 'BACKPACK'
+                  SELECT '가방 : ' || COALESCE(name_kr, name_en), '/item/'|| tkl_item.url_mapping, tkl_item.url_mapping, 'BACKPACK'
                   FROM tkl_item
                   WHERE category = 'Backpack'
                   UNION ALL
-                  SELECT '의료품 : ' || COALESCE(name_kr, name_en), '/itemd/detail/' || tkl_item.url_mapping, tkl_item.url_mapping, 'MEDICAL'
+                  SELECT '의료품 : ' || COALESCE(name_kr, name_en), '/item/' || tkl_item.url_mapping, tkl_item.url_mapping, 'MEDICAL'
                   FROM tkl_item
                   WHERE category = 'Medical'
                   UNION ALL
-                  SELECT '컨테이너 : ' || COALESCE(name_kr, name_en), '/itemd/detail/' || tkl_item.url_mapping, tkl_item.url_mapping, 'CONTAINER'
+                  SELECT '컨테이너 : ' || COALESCE(name_kr, name_en), '/item/' || tkl_item.url_mapping, tkl_item.url_mapping, 'CONTAINER'
                   FROM tkl_item
                   WHERE category = 'Container'
                   UNION ALL
-                  SELECT '열쇠 : ' || COALESCE(name_kr, name_en), '/itemd/detail/' || tkl_item.url_mapping, tkl_item.url_mapping, 'KEY'
+                  SELECT '열쇠 : ' || COALESCE(name_kr, name_en), '/item/' || tkl_item.url_mapping, tkl_item.url_mapping, 'KEY'
                   FROM tkl_item
                   WHERE category = 'Key'
                   UNION ALL
-                  SELECT '식량 : ' || COALESCE(name_kr, name_en), '/itemd/detail/' || tkl_item.url_mapping, tkl_item.url_mapping, 'PROVISIONS'
+                  SELECT '식량 : ' || COALESCE(name_kr, name_en), '/item/' || tkl_item.url_mapping, tkl_item.url_mapping, 'PROVISIONS'
                   FROM tkl_item
                   WHERE category = 'Provisions'
                   UNION ALL
-                  SELECT '전리품 : ' || COALESCE(name_kr, name_en), '/itemd/detail/' || tkl_item.url_mapping, tkl_item.url_mapping, 'LOOT'
+                  SELECT '전리품 : ' || COALESCE(name_kr, name_en), '/item/' || tkl_item.url_mapping, tkl_item.url_mapping, 'LOOT'
                   FROM tkl_item
                   WHERE category = 'Loot'
                   UNION ALL
-                  SELECT '얼굴 커버 : ' || COALESCE(name_kr, name_en), '/itemd/detail/' || tkl_item.url_mapping, tkl_item.url_mapping, 'FACE_COVER'
+                  SELECT '얼굴 커버 : ' || COALESCE(name_kr, name_en), '/item/' || tkl_item.url_mapping, tkl_item.url_mapping, 'FACE_COVER'
                   FROM tkl_item
                   WHERE category = 'FaceCover'
                   UNION ALL
-                  SELECT '완장 : ' || COALESCE(name_kr, name_en), '/itemd/detail/' || tkl_item.url_mapping, tkl_item.url_mapping, 'ARM_BAND'
+                  SELECT '완장 : ' || COALESCE(name_kr, name_en), '/item/' || tkl_item.url_mapping, tkl_item.url_mapping, 'ARM_BAND'
                   FROM tkl_item
                   WHERE category = 'Armband'
                   UNION ALL
-                  SELECT '안경 : ' || COALESCE(name_kr, name_en), '/itemd/detail/' || tkl_item.url_mapping, tkl_item.url_mapping, 'GLASSES'
+                  SELECT '안경 : ' || COALESCE(name_kr, name_en), '/item/' || tkl_item.url_mapping, tkl_item.url_mapping, 'GLASSES'
                   FROM tkl_item
                   WHERE category = 'Glasses'
               ) AS subquery
