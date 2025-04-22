f2200faddf82275d38073fe43090cb5a2c1cdc52
poeynus <poeynus@gmail.com>
2025-04-22 14:33:08 +0900

    item price v2 개발중

--- plugins/custom_module/item_price/price_func.py
+++ plugins/custom_module/item_price/price_func.py
@@ -17,7 +17,7 @@ def price_list_process(item_en, item_ko, item_ja):
     }
     width = item_en.get("width")
     height = item_en.get("height")
-    category = categorize_item(item_en.get("category"))
+    category = categorize_item(item_en["category"].get('name'))
     historicalPrices = item_en.get("historicalPrices")
     image = item_en.get("gridImageLink")
     merged_sell_for = []
