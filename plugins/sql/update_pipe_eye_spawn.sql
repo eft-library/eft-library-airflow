3c5a3179b87da4816f2df537939f601ad35555cd
poeynus <poeynus@gmail.com>
2025-05-05 11:29:56 +0900

    boss 수정

--- plugins/sql/update_pipe_eye_spawn.sql
+++ plugins/sql/update_pipe_eye_spawn.sql
@@ -1,7 +1,8 @@
 UPDATE boss_i18n
-SET spawn_chance = (
-  SELECT spawn_chance
-  FROM boss_i18n
-  WHERE id = 'bossKnight'
-)
+SET spawn_chance = (SELECT spawn_chance
+                    FROM boss_i18n
+                    WHERE id = 'bossKnight'),
+    spawn_map    = (SELECT spawn_chance
+                    FROM boss_i18n
+                    WHERE id = 'bossKnight')
 WHERE id in ('followerBigPipe', 'followerBirdEye');
