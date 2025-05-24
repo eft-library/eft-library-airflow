355bbc25d42ec7ab2546f28695095e2fd4418c3f
poeynus <poeynus@gmail.com>
2025-05-24 10:33:25 +0900

    내용 수정

--- dags/dags_health_check.py
+++ dags/dags_health_check.py
@@ -72,8 +72,8 @@ def get_email_body(ti):
 send_email = EmailOperator(
     task_id="send_email",
     to=["poeynus@gmail.com"],
-    cc=["moonjipsa@gmail.com"],
-    subject="🚨 EFT Library 서비스에 문제가 생겼습니다.",
+    cc=["moonjipsa@gmail.com", "jjy2mn@gmail.com"],
+    subject="🚨 테스트용입니다. (이선엽)",
     html_content="{{ task_instance.xcom_pull(task_ids='prepare_email_body', key='email_body') }}",
     dag=dag,
     conn_id="smtp_gmail",
