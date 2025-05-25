e241fc1738a90e885b89af9555780a5c8c36ee56
poeynus <poeynus@gmail.com>
2025-05-25 11:28:19 +0900

    health check 완료

--- dags/dags_health_check.py
+++ dags/dags_health_check.py
@@ -64,7 +64,7 @@ with DAG(
         task_id="send_email",
         to=["poeynus@gmail.com"],
         cc=["moonjipsa@gmail.com", "jjy2mn@gmail.com"],
-        subject="🚨 테스트용입니다. (이선엽)",
+        subject="🚨 EFT Library 서비스에 문제가 생겼습니다.",
         html_content="{{ task_instance.xcom_pull(task_ids='prepare_email_body', key='email_body') }}",
         conn_id="smtp_gmail",
     )
