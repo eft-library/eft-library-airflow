7cfbc13a7a7a72ee6e9b96154ce483e7c7e2ce2f
poeynus <poeynus@gmail.com>
2025-06-02 08:54:53 +0900

    item detail dag 병렬로 수정

--- dags/dags_item_detail_upsert.py
+++ dags/dags_item_detail_upsert.py
@@ -1,6 +1,7 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.utils.task_group import TaskGroup
 from contextlib import closing
 import pendulum
 from custom_module.psql_func import read_sql
@@ -14,12 +15,13 @@ default_args = {
 }
 
 with DAG(
-    dag_id="dags_item_detail_upsert",
+    dag_id="dags_item_detail_upsert_parallel",
     default_args=default_args,
     start_date=pendulum.datetime(2024, 5, 1, tz="Asia/Seoul"),
     schedule_interval="50 0 * * *",
     tags=["postgresql", "tarkov-dev-api"],
     catchup=False,
+    max_active_tasks=10,  # 병렬 작업 수 제한 가능
 ) as dag:
 
     def get_total_count(postgres_conn_id, **context):
@@ -30,35 +32,47 @@ with DAG(
                 cursor.execute(sql)
                 total_count = cursor.fetchone()[0]
             conn.commit()
-
         return total_count
 
-    def process_batch(postgres_conn_id, **kwargs):
-        ti = kwargs["ti"]
-        total_count = ti.xcom_pull(task_ids="get_total_count")
-
+    def process_batch(postgres_conn_id, start: int, end: int, **kwargs):
         postgres_hook = PostgresHook(postgres_conn_id)
         sql = read_sql("item_detail_upsert.sql")
-
         with closing(postgres_hook.get_conn()) as conn:
             with closing(conn.cursor()) as cursor:
-                for start in range(0, total_count, BATCH_SIZE):
-                    end = start + BATCH_SIZE
-                    cursor.execute(sql, (start, end))
+                cursor.execute(sql, (start, end))
             conn.commit()
 
     get_total_count_task = PythonOperator(
         task_id="get_total_count",
         python_callable=get_total_count,
         op_kwargs={"postgres_conn_id": "tkl_db"},
+    )
+
+    def generate_batch_tasks(**kwargs):
+        ti = kwargs["ti"]
+        total_count = ti.xcom_pull(task_ids="get_total_count")
+
+        with TaskGroup(group_id="batch_upserts") as tg:
+            for start in range(0, total_count, BATCH_SIZE):
+                end = start + BATCH_SIZE
+                PythonOperator(
+                    task_id=f"process_batch_{start}_{end}",
+                    python_callable=process_batch,
+                    op_kwargs={
+                        "postgres_conn_id": "tkl_db",
+                        "start": start,
+                        "end": end,
+                    },
+                )
+        return tg
+
+    from airflow.operators.python import PythonVirtualenvOperator
+
+    # TaskGroup을 동적으로 생성하는 작업
+    generate_tasks = PythonOperator(
+        task_id="generate_batches",
+        python_callable=generate_batch_tasks,
         provide_context=True,
     )
 
-    process_batch_task = PythonOperator(
-        task_id="process_batch",
-        python_callable=process_batch,
-        op_kwargs={"postgres_conn_id": "tkl_db"},
-        provide_context=True,
-    )
-
-    get_total_count_task >> process_batch_task
+    get_total_count_task >> generate_tasks
