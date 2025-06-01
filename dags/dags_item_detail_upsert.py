4ad77b10aeb9b663d471006b619ec4993f1784a2
poeynus <poeynus@gmail.com>
2025-06-02 08:37:14 +0900

    item detail dag 수저

--- dags/dags_item_detail_upsert.py
+++ dags/dags_item_detail_upsert.py
@@ -7,7 +7,6 @@ import pendulum
 from custom_module.psql_func import read_sql
 
 BATCH_SIZE = 500
-MAX_BATCHES = 10  # 최대 10개 batch로 가정
 
 default_args = {
     "owner": "airflow",
@@ -33,35 +32,34 @@ with DAG(
                 total_count = cursor.fetchone()[0]
             conn.commit()
 
-        context["ti"].xcom_push(key="total_count", value=total_count)
+        return total_count
 
-    def process_batch(postgres_conn_id, offset_count, limit, **kwargs):
+    def process_batch(postgres_conn_id, **kwargs):
+        ti = kwargs["ti"]
+        total_count = ti.xcom_pull(task_ids="get_total_count")
+
         postgres_hook = PostgresHook(postgres_conn_id)
         sql = read_sql("item_detail_upsert.sql")
+
         with closing(postgres_hook.get_conn()) as conn:
             with closing(conn.cursor()) as cursor:
-                cursor.execute(sql, {"offset_count": offset_count, "limit": limit})
+                for start in range(0, total_count, BATCH_SIZE):
+                    end = start + BATCH_SIZE
+                    cursor.execute(sql, {"offset_count": start, "limit": end})
             conn.commit()
-        print(f"Processed batch {offset_count} ~ {offset_count + limit}")
 
-    task_get_total_count = PythonOperator(
+    get_total_count_task = PythonOperator(
         task_id="get_total_count",
         python_callable=get_total_count,
         op_kwargs={"postgres_conn_id": "tkl_db"},
+        provide_context=True,
     )
 
-    with TaskGroup(group_id="batch_tasks") as batch_tasks:
-        for i in range(MAX_BATCHES):
-            offset = i * BATCH_SIZE
-            task = PythonOperator(
-                task_id=f"process_batch_{i}",
-                python_callable=process_batch,
-                op_kwargs={
-                    "postgres_conn_id": "tkl_db",
-                    "offset_count": offset,
-                    "limit": BATCH_SIZE,
-                },
-            )
-            task_get_total_count >> task
+    process_batch_task = PythonOperator(
+        task_id="process_batch",
+        python_callable=process_batch,
+        op_kwargs={"postgres_conn_id": "tkl_db"},
+        provide_context=True,
+    )
 
-    task_get_total_count >> batch_tasks
+    get_total_count_task >> process_batch_task
