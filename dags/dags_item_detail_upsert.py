c76053814c9ee878e628e927986166d3a66c705d
poeynus <poeynus@gmail.com>
2025-06-04 08:24:23 +0900

    item detail dag 수정

--- dags/dags_item_detail_upsert.py
+++ dags/dags_item_detail_upsert.py
@@ -15,7 +15,7 @@ default_args = {
 }
 
 with DAG(
-    dag_id="dags_item_detail_upsert_parallel",
+    dag_id="dags_item_detail_upsert",
     default_args=default_args,
     start_date=pendulum.datetime(2024, 5, 1, tz="Asia/Seoul"),
     schedule_interval="50 0 * * *",
