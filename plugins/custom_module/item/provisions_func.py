01160e4b0f3c284f45f9715bd2b4b38367ac4ffd
poeynus <poeynus@gmail.com>
2025-07-24 19:49:30 +0900

    효과 수정

--- plugins/custom_module/item/provisions_func.py
+++ plugins/custom_module/item/provisions_func.py
@@ -317,51 +317,23 @@ def v2_provisions_process(item_en, item_ko, item_ja):
     malus = []
 
     for effect in result_stim_effects:  # effects는 JSON에서 불러온 dict list
-        effect_type = effect["type"]
-        effect_skill = effect["skill_name_en"]
+        effect_info = effect["skill_name_en"]
         value = effect.get("value", 0)
 
-        type_info = effect_dict.get(effect_type)
-        skill_info = effect_dict.get(effect_skill)
-
-        if not type_info:
+        if not effect_info:
             continue  # 정의되지 않은 타입은 무시
 
         # 1. advantage / malus 분류
-        if type_info["is_positive"]:
+        if effect_info["is_positive"]:
             advantage.append(effect)
         else:
             malus.append(effect)
 
         # 2. buff / de_buff 분류
-        if not type_info["is_use_value"]:
+        if not effect_info["is_use_value"]:
             continue  # 값이 없으면 판단 불가
 
-        if type_info["is_positive"]:
-            if value > 0:
-                buff.append(effect)
-            elif value < 0:
-                de_buff.append(effect)
-        else:
-            if value > 0:
-                de_buff.append(effect)
-            elif value < 0:
-                buff.append(effect)
-
-        if not skill_info:
-            continue  # 정의되지 않은 타입은 무시
-
-        # 1. advantage / malus 분류
-        if skill_info["is_positive"]:
-            advantage.append(effect)
-        else:
-            malus.append(effect)
-
-        # 2. buff / de_buff 분류
-        if not skill_info["is_use_value"]:
-            continue  # 값이 없으면 판단 불가
-
-        if skill_info["is_positive"]:
+        if effect_info["is_positive"]:
             if value > 0:
                 buff.append(effect)
             elif value < 0:
