76b1c21d2252f1e991773fe049e53336d58530ed
poeynus <poeynus@gmail.com>
2025-08-22 10:49:46 +0900

    좋아요 집계 로직 수정

--- plugins/sql/delete_issue_posts.sql
+++ plugins/sql/delete_issue_posts.sql
@@ -1,10 +1,15 @@
 DELETE FROM community_posts_hot_issue h
-USING (
-    SELECT cpr.post_id
+USING community_posts cp
+LEFT JOIN (
+    SELECT cpr.post_id,
+           SUM(CASE WHEN cpr.reaction_type = 1 THEN 1 ELSE 0 END) -
+           SUM(CASE WHEN cpr.reaction_type = 0 THEN 1 ELSE 0 END) AS reaction_diff
     FROM community_posts_reactions cpr
     GROUP BY cpr.post_id
-    HAVING
-        SUM(CASE WHEN cpr.reaction_type = 1 THEN 1 ELSE 0 END) -
-        SUM(CASE WHEN cpr.reaction_type = 0 THEN 1 ELSE 0 END) < 10
-) t
-WHERE h.post_id = t.post_id;
+) t ON h.post_id = t.post_id
+WHERE h.post_id = cp.id
+  AND (
+        cp.delete_by_user = true
+        OR cp.delete_by_admin = true
+        OR (cp.delete_by_user = false AND cp.delete_by_admin = false AND t.reaction_diff < 10)
+      );
--- plugins/sql/update_issue_posts.sql
+++ plugins/sql/update_issue_posts.sql
@@ -1,6 +1,10 @@
 INSERT INTO community_posts_hot_issue (post_id, issue_time)
 SELECT cpr.post_id, now()
 FROM community_posts_reactions cpr
+JOIN community_posts cp
+  ON cpr.post_id = cp.id
+WHERE cp.delete_by_user = false
+  AND cp.delete_by_admin = false
 GROUP BY cpr.post_id
 HAVING
     SUM(CASE WHEN cpr.reaction_type = 1 THEN 1 ELSE 0 END) -
