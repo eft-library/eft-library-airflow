ca25b60478821a0c65e05f0c20178afeb5b949ef
poeynus <poeynus@gmail.com>
2026-03-04 14:56:54 +0900

    story rag dag 추가

---
+++ dags/dags_rag_story_embed.py
@@ -0,0 +1,28 @@
+from airflow import DAG
+import pendulum
+from airflow.providers.standard.operators.python import PythonOperator
+from custom_module.rag.rag_story_func import run_story_rag_embed
+
+default_args = {
+    "owner": "airflow",
+    "retries": 1,
+    "retry_delay": pendulum.duration(minutes=5),
+}
+
+with DAG(
+    dag_id="dags_rag_story_embed",
+    default_args=default_args,
+    start_date=pendulum.datetime(2026, 1, 1, tz="Asia/Seoul"),
+    schedule=None,
+    catchup=False,
+    tags=["rag", "story"],
+) as dag:
+
+    embed_task = PythonOperator(
+        task_id="embed_story_i18n",
+        python_callable=run_story_rag_embed,
+        op_kwargs={
+            "postgres_conn_id": "tkl_db",
+            "batch_size": 10,
+        },
+    )
